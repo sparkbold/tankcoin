70c41c12d46f6b8af2e151a2e3c3989e457ac393
Flatiron School <andrewsprague@outlook.com>
2018-10-23 16:10:10 -0500

    update pricesand games controller

--- tank-trader-backend/app/models/price.rb
+++ tank-trader-backend/app/models/price.rb
@@ -6,12 +6,13 @@ class Price < ApplicationRecord
         price = 0
         if event == nil 
             # use the normal distribution to add the price
-            sigma = 1
-            mean = 0
-            rand_number = rand()
-            exponent = (rand_number - mean)**2 / (2 * (sigma**2))
-            beta = 1 / ((2 * Math::PI**0.5) * sigma)
-            new_number = (Math.exp(-exponent) * beta)
+            # sigma = 1
+            # mean = 0
+            # rand_number = rand()
+            # exponent = (rand_number - mean)**2 / (2 * (sigma**2))
+            # beta = 1 / ((2 * Math::PI**0.5) * sigma)
+            # new_number = (Math.exp(-exponent) * beta)
+            new_number = (rand() - 1) / 100
             price = new_number
         else
             #  use the event mean/sigma to add the price
