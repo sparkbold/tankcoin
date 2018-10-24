e42bd77623e55d9a2be96e6209fdad96904b1fd8
Flatiron School <andrewsprague@outlook.com>
2018-10-24 09:39:19 -0500

    add changes

--- tank-trader-backend/app/controllers/games_controller.rb
+++ tank-trader-backend/app/controllers/games_controller.rb
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
-# frozen_string_literal: true
 
->>>>>>> 8d8247cc97a43eddcc574cb9598d776beae5766e
+
 class GamesController < ApplicationController
   def index
     games = Game.all
@@ -10,14 +7,10 @@ class GamesController < ApplicationController
   end
 
   def create
-<<<<<<< HEAD
     game_params = user_params
+    binding.pry
     new_game = Game.create(game_params.user_name)
     new_game.create_events(game_params.num_of_events)
-=======
-    new_game = Game.create(user_id: 8)
-    new_game.create_events(4)
->>>>>>> 8d8247cc97a43eddcc574cb9598d776beae5766e
     new_game.create_prices
 
     render json: { prices: new_game.prices, events: new_game.events }
