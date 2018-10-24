e0e862772035bda8cd8586178d2d69d6257a4eb0
Flatiron School <andrewsprague@outlook.com>
2018-10-24 09:44:27 -0500

    switching branches to see what I missed

--- tank-trader-backend/app/controllers/games_controller.rb
+++ tank-trader-backend/app/controllers/games_controller.rb
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-class GamesController < ApplicationController
-    
-    def index
-        games = Game.all
-        render json: {games: games}
-    end
-
-    def create
-        new_game = Game.create(user_id: 5)
-        new_game.create_events(4)
-        new_game.create_prices
-=======
-# frozen_string_literal: true
->>>>>>> master
-
 class GamesController < ApplicationController
   def index
     games = Game.all
@@ -25,8 +9,7 @@ class GamesController < ApplicationController
     new_game.create_events(4)
     new_game.create_prices
 
-    render json: { prices: self.new_game.prices, events: self.new_game.events }
+    render json: { prices: new_game.prices, events: new_game.events }
   end
 
-  def show; end
 end
