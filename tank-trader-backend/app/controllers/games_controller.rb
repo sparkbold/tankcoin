7929dbdb2c57e3dcbddfa082b82e60f0fd65a5c9
Flatiron School <andrewsprague@outlook.com>
2018-10-24 17:49:16 -0500

    attempted PATCH update for game

--- tank-trader-backend/app/controllers/games_controller.rb
+++ tank-trader-backend/app/controllers/games_controller.rb
@@ -5,6 +5,10 @@ class GamesController < ApplicationController
     games = Game.all
     render json: { games: games }
   end
+  def show
+    game = Game.find(params[:id])
+    render json: {game: game}
+  end
 
   def create
     game_params = user_params
@@ -13,12 +17,22 @@ class GamesController < ApplicationController
     new_game.create_events(game_params['num_of_events'])
     new_game.create_prices
 
-    render json: { prices: new_game.prices, events: new_game.events, game_events: new_game.game_events }
+    render json: { game_id: new_game.id, prices: new_game.prices, events: new_game.events, game_events: new_game.game_events }
+  end
+
+  def update
+    puts params
+    gp = game_params
+    Game.update(id: game_params.id, net_value: game_params.net_value, end_price: game_params.end_price)
   end
 
   private
 
   def user_params
     params.permit(:user_name, :num_of_events)
+  end
+
+  def game_params
+    params.require(:game).permit(:net_value, :end_price, :id)
   end
 end
